1b7ae07353e8416453a198c44cd9ff820327c64c
Amneris Calle <amneris.calle@gmail.com>
2026-06-03 12:50:42 -0300

    [ADDED] asambleas

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -10,9 +10,9 @@ export default function HomePage() {
         </h1>
         <p className="max-w-2xl text-md text-fd-muted-foreground text-balance">
           Bienvenidos a la Documentación para editores y creadores de contenido
-          para el sitio de la AOA. En estas páginas encontrarás información
-          sobre cómo crear páginas, agregar, editar y borrar datos, y mantener
-          el sitio actualizado SIEMPRE.
+          del sitio de la AOA. En estas páginas encontrarás información sobre
+          cómo crear páginas, agregar, editar y borrar datos, y mantener el
+          sitio actualizado SIEMPRE.
         </p>
         <h2 className="text-3xl font-bold">¿Listo/a para empezar?</h2>
         <Image
@@ -32,15 +32,16 @@ export default function HomePage() {
             Ir a la documentación
           </Link>
           <Link
-            href="https://github.com/your-org/noqtadocs"
+            href="https://github.com/Amneweb/aoadocs"
             className="rounded-md border px-6 py-3 font-medium"
           >
-            View on GitHub
+            Repo de esta Documentación
           </Link>
         </div>
         <p className="text-sm max-w-xl text-fd-muted-foreground">
           Si sos afiliado y llegaste acá buscando información sobre cómo
-          realizar trámites, este no es el lugar que buscabas. Andá a{" "}
+          realizar trámites, este no es el lugar que buscabas. Te sugerimos
+          visitar la{" "}
           <a
             href="http://76.13.227.142/aoa/tramites"
             className="underline underline-offset-4 hover:no-underline"
